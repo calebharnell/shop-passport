910307da993befc3c1697962c2801d17ea3cc997
calebharnell <calebharnell@hotmail.com>
2017-12-06 11:51:05 +1000

    Add error handling

--- api/server.js
+++ api/server.js
@@ -1,21 +1,32 @@
 // Bring in environment variables
 if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
+  require('dotenv').config();
 }
 
-const express = require('express')
-const bodyParser = require('body-parser')
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
 // Plugins
 app.use(bodyParser.json()) // Allows me to have JSON uploads (POST/PUT)
 
-// Routes
+// JSON error handling
+app.use((error, req, res, next) => {
+  res.send({ error: error.message })
+});
 
+app.use((req, res, next) => {
+  // No other routes left, must be a 404
+  res.status(404).send({
+     error: `No route found for ${req.method}, ${req.url}`
+   })
+});
+
+// Routes
 app.listen(7000, (error) => {
   if (error) {
     console.log('There was a problem starting the server', error)
   } else {
     console.log('Server is listening on http://localhost:7000/')
   }
-})
+});
