8ec4ce305b77833ffa8d60d6fd314b64d50ab4e1
calebharnell <calebharnell@hotmail.com>
2017-12-06 14:27:29 +1000

    Implement Authentication

--- api/models/User.js
+++ api/models/User.js
@@ -1,17 +1,17 @@
 const mongoose = require('./init');
 const passportLocalMongoose = require('passport-local-mongoose');
 
-const userSchema = new mongoose.schema({
+const userSchema = new mongoose.Schema({
   firstName: String,
   lastName: String
 })
 
-userShema.plugin(passportLocalMongoose, {
+userSchema.plugin(passportLocalMongoose, {
   usernameField: 'email', // Use email not username
   usernameLowerCase: true, // Treat emails as case-sensitive
   session: false // We'll use JWT
 })
 
-const User = mongoose.model('User');
+const User = mongoose.model('User', userSchema);
 
 module.exports = User;
