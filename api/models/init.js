c5f852f9eef028cb57bdaafe129b99e3414ff00b
calebharnell <calebharnell@hotmail.com>
2017-12-06 12:03:46 +1000

    Product model, seed and drop

--- api/models/init.js
+++ api/models/init.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 // Use the promise functionality built into Node
-mongoose.Promise = global.promise
+mongoose.Promise = global.Promise
 
 mongoose.connect('mongodb://localhost/myshop', { useMongoClient: true })
   .then(() => {
     console.log('Succesfully connected to database')
   })
-  .catch(error => {
+  .catch((error) => {
     // Something went wrong
     console.log('Error connecting to MongoDB database', error)
   })
